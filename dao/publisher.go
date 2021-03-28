9afbe06d33d1fa232378d43d2b08773bf0cd2408
leprosus <admin@leprosus.ru>
2021-03-28 11:56:17 +0200

    Fix: dao asin publish

--- dao/publisher.go
+++ dao/publisher.go
@@ -8,7 +8,7 @@ import (
 func (a ASIN) Publish() (err error) {
 	err = conn.WithSQL(func(tx *sqlx.Tx) (err error) {
 		query := `update catalog.unit set is_published = true where asin = ?;`
-		_, err = tx.Exec(query, a)
+		_, err = tx.Exec(query, string(a))
 
 		return
 	})
